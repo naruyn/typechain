5116bebe67104ddcae73357506ec3ec1ccaa10b8
naruyn <naruyn@naver.com>
2021-01-06 14:22:27 +0900

    study typescript

--- dist/index.js
+++ dist/index.js
@@ -11,9 +11,46 @@ class Block {
     }
 }
 Block.calcBlockHash = (index, prevHash, data, timestamp) => CryptoJS.SHA256(index + prevHash + timestamp + data).toString();
+Block.validBlock = (checkBlock) => typeof checkBlock.index === "number" &&
+    typeof checkBlock.hash === "string" &&
+    typeof checkBlock.prevHash === "string" &&
+    typeof checkBlock.data === "string" &&
+    typeof checkBlock.timestamp === "number";
 const genesisBlock = new Block(0, "hash", "prevHas", "data", 123456);
 const blockchain = [genesisBlock];
 const getBlockchain = () => blockchain;
 const getLastBlockchain = () => blockchain[blockchain.length - 1];
 const getNewTimestamp = () => Math.round(new Date().getTime() / 1000);
+const createBlock = (data) => {
+    const prevBlock = getLastBlockchain();
+    const newIndex = prevBlock.index + 1;
+    const newTimestamp = getNewTimestamp();
+    const newHash = Block.calcBlockHash(newIndex, prevBlock.hash, data, newTimestamp);
+    const newBlock = new Block(newIndex, newHash, prevBlock.hash, data, newTimestamp);
+    addBlock(newBlock);
+    return newBlock;
+};
+const getHashForBlock = (targetBlock) => Block.calcBlockHash(targetBlock.index, targetBlock.prevHash, targetBlock.data, targetBlock.timestamp);
+const isVaildBlock = (candidateBlock, prevBlock) => {
+    if (!Block.validBlock(candidateBlock)) {
+        return false;
+    }
+    else if (prevBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    }
+    else if (prevBlock.hash !== candidateBlock.prevHash) {
+        return false;
+    }
+    else if (getHashForBlock(candidateBlock) !== candidateBlock.hash) {
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+const addBlock = (candidateBlock) => {
+    if (isVaildBlock(candidateBlock, getLastBlockchain())) {
+        blockchain.push(candidateBlock);
+    }
+};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,13 @@ class Block {
 	static calcBlockHash = (index: number, prevHash: string, data: string, timestamp: number): string =>
 		CryptoJS.SHA256(index + prevHash + timestamp + data).toString();
 
+	static validBlock = (checkBlock: Block) =>
+		typeof checkBlock.index === "number" &&
+		typeof checkBlock.hash === "string" &&
+		typeof checkBlock.prevHash === "string" &&
+		typeof checkBlock.data === "string" &&
+		typeof checkBlock.timestamp === "number";
+
 	constructor(index: number, hash: string, prevHash: string, data: string, timestamp: number) {
 		this.index = index;
 		this.hash = hash;
@@ -28,5 +35,38 @@ const getBlockchain = (): Block[] => blockchain;
 const getLastBlockchain = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createBlock = (data: string): Block => {
+	const prevBlock: Block = getLastBlockchain();
+	const newIndex: number = prevBlock.index + 1;
+	const newTimestamp: number = getNewTimestamp();
+	const newHash: string = Block.calcBlockHash(newIndex, prevBlock.hash, data, newTimestamp);
+	const newBlock: Block = new Block(newIndex, newHash, prevBlock.hash, data, newTimestamp);
+	addBlock(newBlock);
+	return newBlock;
+};
+
+const getHashForBlock = (targetBlock: Block): string =>
+	Block.calcBlockHash(targetBlock.index, targetBlock.prevHash, targetBlock.data, targetBlock.timestamp);
+
+const isVaildBlock = (candidateBlock: Block, prevBlock: Block): boolean => {
+	if (!Block.validBlock(candidateBlock)) {
+		return false;
+	} else if (prevBlock.index + 1 !== candidateBlock.index) {
+		return false;
+	} else if (prevBlock.hash !== candidateBlock.prevHash) {
+		return false;
+	} else if (getHashForBlock(candidateBlock) !== candidateBlock.hash) {
+		return false;
+	} else {
+		return true;
+	}
+};
+
+const addBlock = (candidateBlock: Block): void => {
+	if (isVaildBlock(candidateBlock, getLastBlockchain())) {
+		blockchain.push(candidateBlock);
+	}
+};
 
 export {};
